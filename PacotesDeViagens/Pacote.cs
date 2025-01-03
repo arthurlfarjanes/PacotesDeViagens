3b83f37dd693cf20ce901c58ea6735575fba8556
Arthur Lanzilotti Farjanes <arthurlfarjanes@gmail.com>
2025-01-03 11:52:59 -0300

    Adicionando validações nas classes Pacote e Reserva + Método CalcularValorTotal em Reserva

--- PacotesDeViagens/Pacote.cs
+++ PacotesDeViagens/Pacote.cs
@@ -16,30 +16,62 @@ namespace PacotesDeViagens
         private int _quantidadeDisponivel;
         private string _detalhes;
 
+        // Variável que busca a data atual do computador do usuário
+        DateTime DataAtual = DateTime.Now;
+
         public int ID
         {
             get { return _id; }
-            set { _id = value; }
+            set { 
+                if(value < 0)
+                {
+                    throw new ArgumentException("Id inválido! Informe um número maior que 0.");
+                }
+                _id = value;
+            }
         }
         public DateTime Data
         {
             get { return _data; }
-            set { _data = value; }
+            set {
+                if (value < DataAtual) {
+                    throw new ArgumentException("Data inválida! Selecione uma data posterior ou igual à data de hoje.");
+                }
+                _data = value; 
+            }
         }
         public int QuantidadeDeNoites
         {
             get { return _quantidadeDeNoites; }
-            set { _quantidadeDeNoites = value; }
+            set { 
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Dados Inválidos! Insira a quantidade de noites do pacote.");
+                }
+                _quantidadeDeNoites = value; 
+            }
         }
         public double Valor
         {
             get { return _valor; }
-            set { _valor = value; }
+            set { 
+                if (value < 0)
+                {
+                    throw new ArgumentException("Valor Inválido! Insira um valor positivo.");
+                }
+                _valor = value; 
+            }
         }
         public int QuantidadeDisponivel
         {
             get { return _quantidadeDisponivel; }
-            set { _quantidadeDisponivel = value; }
+            set { 
+                if (value < 0)
+                {
+                    throw new ArgumentException("Dados Inválidos! Digite um número positivo.");
+                }
+                _quantidadeDisponivel = value; 
+            }
         }
         public string Detalhes
         {
--- PacotesDeViagens/Reserva.cs
+++ PacotesDeViagens/Reserva.cs
@@ -2,17 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace PacotesDeViagens
 {
     public class Reserva
     {
-        private int _id; //teste
+        private int _id;
         private string _status;
-      
+        private List<Pacote> _pacotes;
+        private string _cpfCliente;
+
 
         public int Id { get { return _id; } set { _id = value; } }
         public string Status { get { return _status; } set { _status = value; } }
+        public List<Pacote> Pacotes { get { return _pacotes; } set {_pacotes = value; } }
+        public string CpfCliente
+        {
+            get { return _cpfCliente; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || !Regex.IsMatch(value, @"^\d{11}$"))
+                {
+                    throw new ArgumentException("CPF inválido. Deve conter exatamente 11 dígitos numéricos.");
+                }
+                _cpfCliente = value;
+            }
+        }
+
+        public Reserva(int id, string status, List<Pacote> pacotes)
+        {
+            Id = id; 
+            Status = status; 
+            Pacotes = pacotes;
+        }
+
+        public double CalcularValorTotal()
+        {
+            double valorTotal = 0;
+
+            foreach (var pacote in Pacotes)
+            {
+                valorTotal += pacote.Valor * pacote.QuantidadeDisponivel;
+            }
+
+            return valorTotal;
+        }
     }
 }
